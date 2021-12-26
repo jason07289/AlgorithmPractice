3ec15c3c64133096e8f649f40e77d4e0d1aba99a
jason07289 <jason07999@naver.com>
2021-12-26 14:09:54 +0900

    B1874 stack

--- Algo/src/d2021_12_25/B1874.java
+++ Algo/src/d2021_12_25/B1874.java
@@ -1,7 +1,50 @@
 package d2021_12_25;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+ * https://www.acmicpc.net/problem/1874
+ * 
+ * 8
+ * 
+ * 4, 3, 6, 8, 7, 5, 2, 1
+ */
 public class B1874 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] nums = new int[n];
+		for(int i=0; i< n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		int arrPointer = 0;
+		StringBuilder sb = new StringBuilder();
+		for(int i =1; i<=n; i++) {
+			stack.push(i);
+			sb.append("+").append(System.getProperty("line.separator"));
+			while(stack.peek() == nums[arrPointer]) {
+				stack.pop();
+				sb.append("-").append(System.getProperty("line.separator"));
+				arrPointer++;
+				if(stack.isEmpty()||arrPointer>=n) {
+					break;
+				}
+			}
+			
+			
+		}
+		if(!stack.isEmpty()) {
+			System.out.println("NO");
+		}else {
+			System.out.println(sb.toString());
+		}
+		
 		
 	}
 }
